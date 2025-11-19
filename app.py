85d5f31eb3451560526f97f9085917039f58a9d5
Nicholas Slavin <slavinnj@gmail.com>
2025-11-19 15:57:17 -0500

    Add Braintrust proxy and remote prompt loading

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from braintrust import init_logger, load_prompt, wrap_openai
+from pprint import pprint
+
+load_dotenv()
+# URL of content to classify
+check_url = os.getenv("URL")
+braintrust_api_key = os.getenv("BRAINTRUST_API_KEY")
+braintrust_project_id = os.getenv("BRAINTRUST_PROJECT_ID")
+
+# logger traces the OpenAI client calls
+logger = init_logger(project="PoliticalSlant", api_key=braintrust_api_key)
+
+# load the prompt from Braintrust, specify a version to load different variants
+prompt_object = load_prompt(project="PoliticalSlant", 
+                            slug="political-slant",
+                            )
+
+# x-bt-parent necessary to trace requests made over the proxy
+# must include 'project_id' to avoid improper SpanComponents error
+client = wrap_openai(OpenAI(
+    base_url="https://api.braintrust.dev/v1/proxy",
+    api_key=braintrust_api_key,
+    default_headers={"x-bt-use-cache": "always", 
+                     "Cache-Control": "max-age=1209600",
+                     "x-bt-parent": f"project_id:{braintrust_project_id}"
+                     },
+))
+prompt = prompt_object.build(input=check_url)
+response = client.chat.completions.create(**prompt)
+pprint(response.choices[0].message.content)
