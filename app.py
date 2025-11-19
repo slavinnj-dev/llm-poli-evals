55dc1d5b935c642192ed80e957c16bb7a2867283
Nicholas Slavin <slavinnj@gmail.com>
2025-11-19 16:32:57 -0500

    Break tools into package, add extract tool to prompt

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from braintrust import init_logger, load_prompt, wrap_openai
 from pprint import pprint
+from tools import extract
 
 load_dotenv()
 # URL of content to classify
@@ -13,9 +14,14 @@ braintrust_project_id = os.getenv("BRAINTRUST_PROJECT_ID")
 # logger traces the OpenAI client calls
 logger = init_logger(project="PoliticalSlant", api_key=braintrust_api_key)
 
+# setup tools
+tool_definition = extract.definition
+tools = [extract]
+
 # load the prompt from Braintrust, specify a version to load different variants
 prompt_object = load_prompt(project="PoliticalSlant", 
                             slug="political-slant",
+                            defaults={"tools": [extract.definition], "tool_choice": "required", "stream": False}
                             )
 
 # x-bt-parent necessary to trace requests made over the proxy
@@ -28,6 +34,6 @@ client = wrap_openai(OpenAI(
                      "x-bt-parent": f"project_id:{braintrust_project_id}"
                      },
 ))
-prompt = prompt_object.build(input=check_url)
+prompt = prompt_object.build(input=check_url, tools=tools)
 response = client.chat.completions.create(**prompt)
 pprint(response.choices[0].message.content)
--- tools/extract.py
+++ tools/extract.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from tavily import TavilyClient
 
-load_dotenv()
+load_dotenv("../")
 tavily_key = os.getenv("TAVILY_API_KEY")
 tavily_client = TavilyClient(api_key=tavily_key)
 braintrust_project_name = os.getenv("BRAINTRUST_PROJECT_NAME")
@@ -13,6 +13,28 @@ project = braintrust.projects.create(braintrust_project_name)
 
 class Args(BaseModel):
     url: str
+
+definition = {
+    "type": "function",
+    "function": {
+      "name": "extract_web_content",
+      "description": "Extracts content from a web page",
+      "parameters": {
+        "required": [
+          "url"
+        ],
+        "additionalProperties": False,
+        "properties": {
+          "url": {
+            "type": "string",
+            "description": "The URL of the web page to extract content from"
+          }
+        },
+        "type": "object"
+      },
+      "strict": True
+    }
+  }
 
 def extract(url: str):
     urls = [url]
